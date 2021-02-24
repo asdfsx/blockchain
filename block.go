2022717b26e0ab791f271191b6a8c93097d54d86
sam.sun <asdfsx@gmail.com>
2021-02-24 09:53:08 +0800

    part_3

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -27,4 +30,30 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 // NewGenesisBlock creates and returns genesis Block
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+// Serialize serializes the block
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/asdfsx/blockchain
 
 go 1.15
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	golang.org/x/sys v0.0.0-20210223095934-7937bea0104d // indirect
+)
